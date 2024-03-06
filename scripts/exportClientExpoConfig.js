5524386d2747c2644e8f508d7572e4fb1e8b5352
thestackdev <shanmukeshwar03@gmail.com>
2024-03-06 14:31:23 +0530

    initial commit

--- scripts/exportClientExpoConfig.js
+++ scripts/exportClientExpoConfig.js
@@ -1,7 +1,7 @@
 const ExpoConfig = require('@expo/config');
 const path = require('path');
 
-const projectDir = path.join(__dirname, '..', '..', 'expo-updates-client');
+const projectDir = path.join(__dirname, '..', '..', 'memli-mobile-app');
 
 const { exp } = ExpoConfig.getConfig(projectDir, {
   skipSDKVersionRequirement: true,
--- scripts/publish.sh
+++ scripts/publish.sh
@@ -5,10 +5,10 @@ do
     esac
 done
 
-cd ../expo-updates-client
+cd ../memli-mobile-app
 npx expo export
-cd ../expo-updates-server
+cd ../memli-updates-server
 rm -rf updates/$directory/
-cp -r ../expo-updates-client/dist/ updates/$directory
+cp -r ../memli-mobile-app/dist/ updates/$directory
 
 node ./scripts/exportClientExpoConfig.js > updates/$directory/expoConfig.json
